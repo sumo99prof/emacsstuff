fc4174473b0fed8fe38efec7d8336b34acee1593
sumo99prof <142334618+sumo99prof@users.noreply.github.com>
2023-10-04 18:24:52 -0500

    Update blood_pressure.el
    According to CHATgpt, setconst does not work automatically.

--- blood_pressure.el
+++ blood_pressure.el
@@ -1,14 +1,14 @@
-(defconst ranges
-  '(;; Low
-    (60 90 "blue")
-    ;; Normal
-    (80 120 "green")
-    ;; Pre hypertension
-    (90 140 "yellow")
-    ;; High stage 1 hypertension
-    (100 160 "orange")
-    ;; High stage 2 hypertension
-    (120 190 "red")))
+(setq ranges
+      '(;; Low
+        (60 90 "blue")
+        ;; Normal
+        (80 120 "green")
+        ;; Prehypertension
+        (90 140 "yellow")
+        ;; High stage 1 hypertension
+        (100 160 "orange")
+        ;; High stage 2 hypertension
+        (120 190 "red")))
 
 (defun cell-color (diastolic systolic)
   (or (and diastolic systolic
