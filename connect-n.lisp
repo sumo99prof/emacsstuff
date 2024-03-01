0b955dea229eca15d7e68fff415f5a81bd242904
sumo99prof <142334618+sumo99prof@users.noreply.github.com>
2024-03-01 12:42:07 -0600

    Update connect-n.lisp

--- connect-n.lisp
+++ connect-n.lisp
@@ -1,7 +1,7 @@
 (defvar *move-number* 0)
-(defparameter *game-board* (make-array '(6 7) :initial-element 0))
-(defparameter *col-count* (array-dimension *game-board* 0))
-(defparameter *row-count* (array-dimension *game-board* 1))
+(defparameter *game-board* (make-array '(6 7) :initial-element :-))
+(defparameter *row-count* (array-dimension *game-board* 0))
+(defparameter *col-count* (array-dimension *game-board* 1))
 (defvar *win-threshold* 4)
 (defpackage :connect-n (:use :cl))
 
@@ -9,25 +9,42 @@
 (defun can-drop-column (col-num)
   (loop for i below *col-count*
         for element = (aref *game-board* i col-num)
-        thereis (equal element 0)))
+        thereis (equal element :-)))
 
 (defun set-column (array col-index new-column)
   (dotimes (i (array-dimension array 0))
     (setf (aref array i col-index) (elt new-column i))))
 
 (defun check-maxnum-in-row (elements curr-sym maxnum)
+  (when  (< (list-length elements) maxnum)
+    (return-from check-maxnum-in-row nil))
   (let ((max-streak 0))
          (let ((current-streak 0))
          (loop for i from 0 to (1- (length elements)) do
-             (let ((current (nth i elements))
-                   (next (nth (1+ i) elements)))
-             (if (eq current curr-sym)
-                  (progn
+             (let ((current (nth i elements)))
+               (if (eql current curr-sym)
+                 (progn
                    (incf current-streak)
                    (setf max-streak (max current-streak max-streak)))
-                  (setf current-streak 0)))))
-        (eq max-streak maxnum)))
-  
+                   (setf current-streak 0)))))
+        (>= max-streak maxnum)))
+
+(defun check-win-vert-horiz ()
+  (let* ((winner nil))
+    (loop for col-num below *col-count* do
+          (let ((col-elements (loop for i below *row-count* collect (aref *game-board* i col-num))))
+            (when (check-maxnum-in-row col-elements :X *win-threshold*)
+              (setf winner 'X))
+            (when (check-maxnum-in-row col-elements :O *win-threshold*)
+              (setf winner 'O))))
+    (loop for row-num below *row-count* do
+          (let ((row-elements (loop for i below *row-count* collect (aref *game-board* row-num i))))
+            (when (check-maxnum-in-row row-elements :X *win-threshold*)
+              (setf winner 'X))
+            (when (check-maxnum-in-row row-elements :O *win-threshold*)
+              (setf winner 'O))))
+    winner))
+
 (defun check-win-vert-horiz ()
   (let* ((winner nil)
          (positive-diagonal (diagonal-order-pos *game-board* *row-count* *col-count*)))
@@ -37,17 +54,18 @@
               (setf 'X winner))
             (when (check-maxnum-in-row col-elements 'O *win-threshold*)
               (setf 'O winner)))))
-    (loop for col-num below *col-count* do
-          (let ((row-elements (loop for i below *row-count* collect (aref *game-board* i col-num))))
-            (when (check-maxnum-in-row row-elements 'X *win-threshold*)
-              (setf 'X winner))
-            (when (check-maxnum-in-row row-elements 'O *win-threshold*)
-              (setf 'O winner)))))
+  (loop for col-num below *col-count* do
+        (let ((row-elements (loop for i below *row-count* collect (aref *game-board* i col-num))))
+          (when (check-maxnum-in-row row-elements 'X *win-threshold*)
+            (setf 'X winner))
+          (when (check-maxnum-in-row row-elements 'O *win-threshold*)
+            (setf 'O winner)))))
+
 
 (defun drop-in-col (col-num player-symbol)
-  (let* ((col-elements (loop for i below *col-count* collect (aref *game-board* i col-num)))
-         (zero-elem (position 0 col-elements :from-end t)))
-    (prognf
+  (let* ((col-elements (loop for i below *row-count* collect (aref *game-board* i col-num)))
+         (zero-elem (position :- col-elements :from-end t)))
+    (progn
       (setf (elt col-elements zero-elem) player-symbol)
       (set-column *game-board* col-num col-elements))))
 
@@ -66,14 +84,18 @@
   ())
 
 (defun game-loop ()
-  (princ "Which row to drop into? ")
   (princ "Valid ones are ")
+  (terpri)
   (princ (remove-if-not #'can-drop-column (loop for n below *row-count* collect n)))
-  (if (evenp *move-number*)
-      (drop-in-col 1 :X)
-      (drop-in-col 1 :O))
+  (let ((current-column (input "Which column to drop into? ")))
+    (terpri)
+    (if (evenp *move-number*)
+        (drop-in-col current-column :X)
+        (drop-in-col current-column :O))
+    ()
   (incf *move-number*)
-  (game-loop))
+  (princ *game-board*)
+  (game-loop)))
 
 (defun init-game-board ()
   (let* ((rows (input "How many rows do you desire?"))
