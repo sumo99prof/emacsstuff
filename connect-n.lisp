d6f527278c5658e1da4215698cb4b1b5f6450156
sumo99prof <142334618+sumo99prof@users.noreply.github.com>
2024-03-25 09:55:17 -0500

    Update connect-n.lisp
    still need to do the rows.

--- connect-n.lisp
+++ connect-n.lisp
@@ -4,6 +4,8 @@
 (defparameter *row-count* (array-dimension *game-board* 0))
 (defparameter *col-count* (array-dimension *game-board* 1))
 (defparameter *win-threshold* 4)
+(defparameter *computer-first* t)
+(ql:quickload :array-operations)
 (defpackage :connect-n (:use :cl))
 
 ;;Thanks grolter
@@ -12,14 +14,75 @@
         for element = (aref *game-board* i col-num)
         thereis (equal element "-")))
 
+(defun row-has-space (row-num)
+  (loop for i below *col-count*
+        for element = (aref *game-board* row-num i)
+        thereis (equal element "-")))
+
 (defun pop-checker (col-num move-number)
   )
 
-;set column and return true if no errors
+(defun repeat-element (element count)
+  "Repeats an element a specified number of times."
+  (loop for i below count collect element))
+
+(defun random-list-picker (possible-cols)
+  (when (null possible-cols) (error "Cannot pick from an empty list"))
+  (let ((random-index (random (length possible-cols))))
+    (aref possible-cols random-index)))
+
+(defun positions (item sequence &key (test #'equal))
+  "Returns a list of all positions of ITEM in SEQUENCE, using TEST for comparison."
+  (loop for element in sequence
+        for index from 0
+        when (funcall test element item)
+        collect index))
+
+(defun search-all (subseq seq curr-seq-pos matches)
+  "This function returns a list of all positions where subseq appears in seq."
+  (unless (>= (length seq) (length subseq))  ;base case
+    (return-from search-all matches))
+  (let ((subseq-length (length subseq))   
+        (subseq-position (search subseq seq))) 
+    (when subseq-position
+      (if matches ;when match is null, we have the first match, else accumulate
+          (progn (setf curr-seq-pos (+ subseq-position subseq-length curr-seq-pos))
+                  (push curr-seq-pos matches))
+                  (push subseq-position matches)))
+      (search-all subseq                    
+                  (subseq seq (+ subseq-position subseq-length)) curr-seq-pos
+                  matches)))
+
+(defun random-move-easy ()
+  "The goal of this AI is to randomly drop a piece in a column, avoiding immediate vertical or horizontal threats of four in a row."
+  (let* ((win-minus-one (- *win-threshold* 1))
+         (all-possible-cols (remove-if-not #'can-drop-column (loop for n below *col-count* collect n)))
+         (human-opp-sym (if *computer-first* "O" "X"))
+         (n-minus-one (repeat-element human-opp-sym win-minus-one))
+         (n-row-column (positions human-opp-sym (flatten (cdr (assoc :columns (check-win-vert-horiz win-minus-one)))) :test #'equal))
+         (n-row-row (positions human-opp-sym (flatten (cdr (assoc :rows (check-win-vert-horiz win-minus-one)))) :test #'equal))
+         (n-row-column (mapcar (lambda (x) (floor (/ x 2))) n-row-column))
+         (n-row-row (mapcar (lambda (x) (floor (/ x 2))) n-row-row))
+         (valid-col-threats (intersection all-possible-cols n-row-column))  ; New variable name
+         (forced-move '()))  ; Move forced-move declaration before using it
+    (when valid-col-threats
+      (loop for column in valid-col-threats
+            do (let* ((col-elements (get-column *game-board* column))
+                      (leftmost-empty (+ (position "-" col-elements :from-end t :test #'equal) 1))
+                      (rightmost-threat (+ leftmost-empty win-minus-one)) ;only the last three elements matter
+                      )
+                 (when (equal n-minus-one (subseq col-elements leftmost-empty rightmost-threat)) (push column forced-move)))))
+    (princ forced-move)))
+
+
 (defun set-column (array col-index new-column)
   (dotimes (i (array-dimension array 0))
     (setf (aref array i col-index) (elt new-column i)))
    t)
+
+(defun get-column (array col-index)
+  "Get the column specified by col-index from the 2D array."
+  (loop for i below *row-count* collect (aref *game-board* i col-index)))
 
 (defun check-maxnum-in-row (elements curr-sym maxnum)
   (unless (< (list-length elements) maxnum)
@@ -35,35 +98,36 @@
     (when (>= max-streak maxnum)
       curr-sym))))
 
-(defun flatten (lists)
-  "Flatten a list like ((O nil) (nil nil) (nil X)) only one layer of nesting"
-  (loop for sublist in lists
-        append sublist))
+(defun flatten (alist)
+  "Flattens a nested association list (alist) to a single level."
+  (cond ((null alist) nil)
+        ((atom (car alist)) (cons (car alist) (flatten (cdr alist))))  
+        (t (append (flatten (car alist)) (flatten (cdr alist)))))) 
 
-(defun populate-winner-list (winner-list symbol-list element-list)
+
+(defun populate-winner-list (winner-list symbol-list element-list w-thresh)
   (loop for element in element-list
         do (push (loop for checker in symbol-list
-                       collect (check-maxnum-in-row element checker *win-threshold*))
+                       collect (check-maxnum-in-row element checker w-thresh))
                  winner-list))
   winner-list)
 
-(defun check-win-vert-horiz ()
-  (let* ((winner nil)
-         (pos-diagonal (diagonal-order-pos *game-board* *row-count* *col-count*))
-         (neg-diagonal (diagonal-order-neg *game-board* *row-count* *col-count*))
-         (winner (populate-winner-list nil *piece-names* neg-diagonal)))
-    (setq winner (populate-winner-list winner *piece-names* pos-diagonal))
-    (loop for col-num below *col-count* do
-          (let ((col-elements (loop for i below *row-count* collect (aref *game-board* i col-num))))
-            (push (loop for checker in *piece-names*
-                        collect (check-maxnum-in-row col-elements checker *win-threshold*))
-                  winner)))
-    (loop for row-num below *row-count* do
-          (let ((row-elements (loop for i below *row-count* collect (aref *game-board* row-num i))))
-            (push (loop for checker in *piece-names*
-                        collect (check-maxnum-in-row row-elements checker *win-threshold*))
-                  winner)))
-    winner))
+  (defun check-win-vert-horiz (w-thresh)
+    "Go through the entire game board with upper, lower, columns, rows etc and make an alist to return"
+    (let* ((winner nil)
+           (col-vals (loop for col-num below *col-count* collect
+                           (let ((col-elements (loop for i below *row-count* collect (aref *game-board* i col-num))))
+                             (loop for checker in *piece-names* collect (check-maxnum-in-row col-elements checker w-thresh)))))
+           (row-vals (loop for row-num below *row-count* collect
+                           (let ((row-elements (loop for i below *row-count* collect (aref *game-board* row-num i))))
+                             (loop for checker in *piece-names* collect (check-maxnum-in-row row-elements checker w-thresh)))))
+           (pos-diagonal (diagonal-order-pos *game-board* *row-count* *col-count*))
+           (neg-diagonal (diagonal-order-neg *game-board* *row-count* *col-count*))
+           (winner (acons :diag-neg (populate-winner-list nil *piece-names* neg-diagonal w-thresh) winner))
+           (winner (acons :diag-pos (populate-winner-list nil *piece-names* pos-diagonal w-thresh) winner))
+           (winner (acons :columns col-vals winner))
+           (winner (acons :rows row-vals winner)))
+      winner))
 
 (defun drop-in-col (col-num player-symbol)
   (let* ((col-elements (loop for i below *row-count* collect (aref *game-board* i col-num)))
@@ -83,10 +147,6 @@
             (progn (princ "Bye") (cl-user::quit))
             (progn (princ "Enter a number, 'quit', or 'exit' please") (input ""))))))
 
-(defun random-move ()
-  (incf *move-number*)
-  ())
-
 (defun game-loop ()
      (princ "Valid ones are ")
      (terpri)
@@ -98,9 +158,9 @@
            (drop-in-col current-column  "O"))
        (incf *move-number*)
        (princ *game-board*)
-       (when (find "X" (flatten (check-win-vert-horiz)))
+       (when (find "X" (flatten (check-win-vert-horiz 4)) :test #'equal)
          (format t "Player one has won! ~%"))
-       (when (find "O" (flatten (check-win-vert-horiz)))
+       (when (find "O" (flatten (check-win-vert-horiz 4)) :test #'equal)
          (format t "Player two has won! ~%"))
        (game-loop)))
 
@@ -138,9 +198,6 @@
                (values-to-check (list (- (+ row col) line) (- row (- line 1)) (- col start-col) row))
                (non-negative-values (remove-if-not #'plusp values-to-check))
                (element-count (apply #'min non-negative-values))
-               (updated-row (if (> start-col 0) 1 row)))
-          (loop for j from 0 below element-count
-                collect (aref matrix (- (+ (min updated-row line) j) 1) (+ start-col j))))))
                (updated-row (if (> start-col 0) 1 row)))
           (loop for j from 0 below element-count
                 collect (aref matrix (- (+ (min updated-row line) j) 1) (+ start-col j))))))
